4fe6a1ca7366cbb3bdf01cf63daef0130df8bb7e
BrayanCahuanaU <brayan.cahuana@ucsm.edu.pe>
2023-08-19 03:25:59 -0500

    Clase Principal base

---
+++ Principal.java
@@ -0,0 +1,23 @@
+public class Pricnipal {
+	Public stactic void main (Strinf[] args) {
+		int vida, rp;
+		boolean fl = false;
+		vida = 0;
+		Scanner sc = new Sanner(System.in);
+		do{
+			vida ++;
+			System.out.prinln("Vida: " + vida + "\t¿Cual es el numero secreto?")
+			rp = sc.nextInt();
+			if(rp == 1234){
+				fl = true;
+			}
+			while (vida < 3 && !fl);
+			if (fl == true){
+				System.out.println("Adivinaste!!!!");
+			}
+			else{
+				System.out.println("ups, perdiste...")
+			}
+		}
+	}
+}
