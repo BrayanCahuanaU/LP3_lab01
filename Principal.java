f1b6a6d972e1087c4dd960a0ce6b041c1f04e693
BrayanCahuanaU <128568972+BrayanCahuanaU@users.noreply.github.com>
2023-08-21 09:55:11 -0600

    Update Principal.java
    Se añadio el metodo de calcula vidas

--- Principal.java
+++ Principal.java
@@ -1,24 +1,65 @@
-public class Pricnipal {
-	Public stactic void main (Strinf[] args) {
+import java.util.Scanner;
+
+public class Principal {
+	public static void main (String[] args) {
 		int vida, rp;
 		boolean fl = false;
 		vida = 0;
-		Scanner sc = new Sanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		do{
-			vida ++;
-			System.out.prinln("Vida numero: " + vida + "\t¿Cual es el numero secreto?")
+			numerovidas(vida);
+			System.out.println("Vida numero: " + vida + "\t¿Cual es el numero secreto?");
 			rp = sc.nextInt();
 			if(rp == 1234){
 				fl = true;
 			}
-			while (vida < 3 && !fl);
+		}
+		while (vida < 3 && !fl);
 			if (fl == true){
 				System.out.println("Adivinaste!");
-				System.out.println("Usted recibira un premio");
+				System.out.println("Ganaste un premio");
+				String salida = Regalo.elegirRegalo(vida);
 			}
 			else{
-				System.out.println("ups, perdiste...")
+				System.out.println("ups, perdiste...");
+			}
+	}	
+	
+	
+	public static int numerovidas(int vida){
+		vida ++;
+		return vida;
+	}
+}import java.util.Scanner;
+
+public class Principal {
+	public static void main (String[] args) {
+		int vida, rp;
+		boolean fl = false;
+		vida = 0;
+		Scanner sc = new Scanner(System.in);
+		do{
+			numerovidas(vida);
+			System.out.println("Vida numero: " + vida + "\t¿Cual es el numero secreto?");
+			rp = sc.nextInt();
+			if(rp == 1234){
+				fl = true;
 			}
 		}
+		while (vida < 3 && !fl);
+			if (fl == true){
+				System.out.println("Adivinaste!");
+				System.out.println("Ganaste un premio");
+				String salida = Regalo.elegirRegalo(vida);
+			}
+			else{
+				System.out.println("ups, perdiste...");
+			}
+	}	
+	
+	
+	public static int numerovidas(int vida){
+		vida ++;
+		return vida;
 	}
 }
